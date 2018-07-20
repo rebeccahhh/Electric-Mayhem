c7ce6b7649a2cbb3610a8db169504464bbdd2440
EC2 Default User <ec2-user@ip-172-31-36-251.us-east-2.compute.internal>
2018-07-20 09:13:42 +0000

    commiting to prevent losing stuff..........

--- git-access.py
+++
@@ -1,8 +0,0 @@
-import PyGithub
-# or using an access token
-g = Github("bc6de7e390d28b8ad7f7b7c325eaa9b64bf59d9a")
-
-#test
-for repo in g.get_user().get_repos():
-    print (repo.name)
-    repo.edit(has_wiki=False)
--- stringParse.py
+++ stringParse.py
@@ -2,28 +2,66 @@ import sys
 import os
 
 '''
-Feature
-E# (estimate)
-A# (actual)
-Summary
+Returns list of strings: [Feature, Estimate, Actual, Summary]
 '''
 
 def stringParse (gitString):
-    feat = ''
-    est = ''
-    act = ''
-    summ = ''
-    return feat, est, act, summ;
-    
+    gitString = " " + gitString.replace("\n","  ")
+    feat, start, stop = beforeAfter(["F:","Feature:","Feat:"],[' '],gitString)
+    if (feat == "SNP"):
+        feat = ''
+    else:
+        if (start == 0):
+            gitString = gitString[stop:]
+        else:
+            gitString = gitString[:start] + gitString[stop:]
+    est, start, stop = beforeAfter(["E:","Estimate:","Est:"],[' '],gitString)
+    if (est == "SNP"):
+        est = ''
+    else:
+        gitString = gitString[:start] + gitString[stop:]
+    act, start, stop = beforeAfter(["A:","Actual:","Act:"],[' '],gitString)
+    if (act == "SNP"):
+        act = ''
+    else:
+        gitString = gitString[:start] + gitString[stop:]
+    return feat.strip(), est.strip(), act.strip(), gitString.strip();
+        
 def beforeAfter (bef, aft, s):
-    start = s.find(bef)
-    start = start + len(bef)
-    stop = s.find(aft)
-    result = s[start:stop]
-    return result;
+    start, stop, begin = -1, -1, 0
+    for keyword in bef:
+        if (s.lower().find(keyword.lower()) == 0 or s.lower().find(" " + keyword.lower()) != -1):
+            begin = s.lower().find(" " + keyword.lower())
+            if(begin == -1):
+                begin = 0
+            start = begin + len(keyword) + 1
+            if (s[start] == " "):
+                start = start + 1
+    for keyword in aft:
+        if (s[start:].find(keyword) != -1):
+            stop = s[start:].find(keyword) + start
+            break
+    if (stop == -1):
+        stop = len(s)
+    if (start == -1):
+        result = "SNP"
+    else:
+        result = s[start:stop].strip()
+    return result, begin, stop;
     
-def main ():
-    print (sys.argv[0])
-    
-    return;
+if __name__ == '__main__':
+    s = "E:5 \nA:\nF:Merge-Rules \nSummary: This is the Summary text."
+    print ("Input 1: \n" + s)
+    print("\nOutput List 1: ")
+    print (stringParse(s))
+    print("-------------------------------------------------")
+    s = "Summary: This is the Summary text.\nEst:5 \nActual: 9000+\nFeature:Merge-Rules"
+    print ("Input 2: \n" + s)
+    print("\nOutput List 2: ")
+    print (stringParse(s))
+    print("-------------------------------------------------")
+    s = "Summary: Title:This is the Summary text.\nE:5\nActual: 9000+\nFeature:Merge-Rules"
+    print ("Input 3: \n" + s)
+    print("\nOutput List 3: ")
+    print (stringParse(s))
     
